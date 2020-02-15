1518b279c7af98ab5cc9f08e5c2f3a9a93c12084
Jayho261 <hophuc@msu.edu>
2020-02-15 13:13:09 -0500

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Jay Ho on 2/15/20.
+//  Copyright © 2020 Jay Ho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
