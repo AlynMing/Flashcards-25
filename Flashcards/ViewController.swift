f3fd13f8af69e62de1bc778a21b5f2855a56194f
Jayho261 <hophuc@msu.edu>
2020-02-21 12:53:44 -0500

    adding optional steps

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -10,15 +10,70 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
+    @IBOutlet weak var card: UIView!
     @IBOutlet weak var answerLabel: UILabel!
     @IBOutlet weak var questionLabel: UILabel!
+    
+    
+    @IBOutlet weak var btnOptionOne: UIButton!
+    @IBOutlet weak var btnOptionTwo: UIButton!
+    @IBOutlet weak var btnOptionThree: UIButton!
+    
     override func viewDidLoad() { //this is what happened when run the app
         super.viewDidLoad()
+        card.layer.cornerRadius = 20.0
+        card.layer.shadowRadius = 15.0
+        card.layer.shadowOpacity = 0.2
+        questionLabel.layer.cornerRadius = 20.0
+        answerLabel.layer.cornerRadius = 20.0
+        questionLabel.isHidden = false
+        answerLabel.isHidden = true
+        questionLabel.clipsToBounds = true
+        answerLabel.clipsToBounds = true
+        btnOptionOne.layer.cornerRadius = 20.0
+        btnOptionTwo.layer.cornerRadius = 20.0
+        btnOptionThree.layer.cornerRadius = 20.0
+        btnOptionOne.clipsToBounds = true
+        btnOptionTwo.clipsToBounds = true
+        btnOptionThree.clipsToBounds = true
+        btnOptionOne.layer.borderWidth = 3.0
+        btnOptionOne.layer.borderColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+        btnOptionTwo.layer.borderWidth = 3.0
+        btnOptionTwo.layer.borderColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+        btnOptionThree.layer.borderWidth = 3.0
+        btnOptionThree.layer.borderColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
         // Do any additional setup after loading the view.
     }
     @IBAction func didTapOnFlashcard(_ sender: Any) {
+        if (answerLabel.isHidden == false){
+            answerLabel.isHidden = true
+            questionLabel.isHidden = false
+            
+        }
+        else if (questionLabel.isHidden == true){
+            questionLabel.isHidden = false
+            answerLabel.isHidden = true;
+        }
+        else if (questionLabel.isHidden == false){
+            answerLabel.isHidden = false
+            questionLabel.isHidden = true
+        }
+        else if (answerLabel.isHidden == true){
+            questionLabel.isHidden = true
+            answerLabel.isHidden = false
+        }
+    }
+    
+    @IBAction func didTapOptionOne(_ sender: Any) {
+        btnOptionOne.isHidden = true
+    }
+    @IBAction func didTapOptionTwo(_ sender: Any) {
         questionLabel.isHidden = true
+        answerLabel.isHidden = false
+    }
+ 
+    @IBAction func didTapOptionThree(_ sender: Any) {
+        btnOptionThree.isHidden = true
     }
 }
 
